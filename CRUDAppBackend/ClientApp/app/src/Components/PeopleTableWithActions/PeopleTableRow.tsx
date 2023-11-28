0648db9f6132caed80e29a63249868bd41722099
IlliaShein <illiasheinillia@gmail.com>
2023-11-28 18:59:36 +0200

    Refactoring:replaced PeopleTableModalsComponent component into PeopleTableActionButtons component, also replaced there deletePerson method

--- CRUDAppBackend/ClientApp/app/src/Components/PeopleTableWithActions/PeopleTableRow.tsx
+++ CRUDAppBackend/ClientApp/app/src/Components/PeopleTableWithActions/PeopleTableRow.tsx
@@ -1,37 +1,12 @@
 import { TableCell, TableRow } from '@mui/material';
 import { Person } from '../../Interfaces/Person';
 import PeopleTableActionButtons from './PeopleTableActionButtons';
-import * as Api from '../../APIs/Api';
-import { useState } from 'react';
-import PeopleTableModalsComponent from './PeopleTableModalsComponent';
-import { CreateContext } from '../../context/context';
-import { GetPersonsContext } from '../../Pages/PeoplePage';
 
 interface PersonRowProps {
   person: Person;
 }
 
 function PeopleTableRow({ person }: PersonRowProps) {
-  const { updatePersons } = CreateContext(GetPersonsContext);
-  const [modalType, setModalType] = useState<string | null>(null);
-  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-
-  const handleOpenModal = (type: string, person: Person) => {
-    setModalType(type);
-    setSelectedPerson(person);
-  };
-
-  const handleCloseModal = () => {
-    setModalType(null);
-    setSelectedPerson(null);
-    updatePersons();
-  };
-
-  const deletePerson = async () => {
-    await Api.RemovePerson(person.id);
-    updatePersons();
-  };
-
   return (
     <TableRow key={person.id}>
       <TableCell>{person.name}</TableCell>
@@ -48,17 +23,8 @@ function PeopleTableRow({ person }: PersonRowProps) {
       )}
       <TableCell>{person.role}</TableCell>
       <TableCell>
-        <PeopleTableActionButtons
-          onEditClick={() => handleOpenModal("edit", person)}
-          onViewClick={() => handleOpenModal("view", person)}
-          onDeleteClick={deletePerson}
-        />
+        <PeopleTableActionButtons person = {person}/>
       </TableCell>
-      <PeopleTableModalsComponent
-        modalType={modalType}
-        selectedPerson={selectedPerson}
-        onClose={handleCloseModal}
-      />
     </TableRow>
   );
 }
